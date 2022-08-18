16990e33b6d38c56dbc964d2db01606cf2459c37
ethanbar11 <ethanbarbar@gmail.com>
2022-08-18 09:22:05 +0300

    Changed helo.py

--- helo.py
+++ helo.py
@@ -1,1 +1,23 @@
 print("hii")
+print("hii")
+print("hii")
+print("hii")
+print("hii")
+print("hii")
+print("hii")
+print("hii")
+print("hii")
+for i in range(500):
+    print(i)
+
+
+# Fibonnachi
+def fib(n):
+    a, b = 0, 1
+    while b < n:
+        print(b, end=' ')
+        a, b = b, a + b
+    print()
+
+
+fib(2000)
